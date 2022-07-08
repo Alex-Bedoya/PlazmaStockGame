76e0222adb6f82459cf6164345ce8b8903a94bdc
Alexander Bedoya <alexanderbedoya@mail.weber.edu>
2022-07-07 20:00:28 -0600

    made Stock Class

---
+++ PlazmaStockGame/Models/Class.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlazmaStockGame.Models
+{
+    public class Stock
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Ticker { get; set; }
+        public DateOnly Date  { get; set; }
+        public double Cost { get; set; }
+
+        public Stock(string ticker, DateOnly date, double cost)
+        {
+            Ticker = ticker;
+            Date = date;
+            Cost = cost;
+        }
+    }
+}

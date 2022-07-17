81aba57452ca0e941f24e161b1045a22cd020772
Ryan D <rdick113@gmail.com>
2022-07-17 16:51:51 -0600

    fixed small issues

--- PlazmaStockGame/DataAccess/StockDbContext.cs
+++ PlazmaStockGame/DataAccess/StockDbContext.cs
@@ -10,18 +10,18 @@ namespace PlazmaStockGame.DataAccess
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
 
-            //optionsBuilder.UseSqlServer(
+            optionsBuilder.UseSqlServer(
             //   @"Server=DESKTOP-27VJS0V;Database=PlazmaStockGame;Trusted_Connection=True;");
             //Brendan
             //Ryan Desktop
-            //@"Server=DESKTOP-D61Q4DT;Database=PlazmaStockGame;Trusted_Connection=True;");
+            @"Server=DESKTOP-D61Q4DT;Database=PlazmaStockGame;Trusted_Connection=True;");
             //Ryan Laptop LAPTOP-E5G80MMC
             //@"Server=LAPTOP-E5G80MMC;Database=PlazmaStockGame;Trusted_Connection=True;");
 
 
             //@"Server=DESKTOP-B6VT6G1\SQLEXPRESS;Database=PlazmaStockGame;Trusted_Connection=True;"); // alex
 
-            optionsBuilder.UseSqlServer("Server=localhost;Database=master;Trusted_Connection=True;"); //Adam
+            //optionsBuilder.UseSqlServer("Server=localhost;Database=master;Trusted_Connection=True;"); //Adam
         }
     }
 }
